ab4a130f7164e0b1dcd746c254ca6992b7d97998
PavelP4 <reginuser@gmail.com>
2015-01-10 17:52:43 +0300

    Up to chapter 10

---
+++ MvcApp1/MvcApp1.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcApp1.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MvcApp1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Product[] products = {
+            new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+            new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+            new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+            new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+        };
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Mock<IValueCalculator> mock = new Mock<IValueCalculator>();
+            mock.Setup(m => m.ValueProducts(It.IsAny<IEnumerable<Product>>()))
+                .Returns<IEnumerable<Product>>(arr => arr.Sum(e => e.Price));
+
+            //var target = new LinqValueCalculator();
+            // act
+            var result = mock.Object.ValueProducts(products);
+            // assert
+            Assert.AreEqual(products.Sum(e => e.Price), result);
+        }
+
+    }
+}
---
+++ MvcApp1/MvcApp1/ViewModels/ListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MvcApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApp1.ViewModels
+{
+    public class ListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
